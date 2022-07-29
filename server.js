c57570e96385908cb4877443ebff6abde9ba22fa
Britt Carlton <brittanyrc95@gmail.com>
2022-07-29 11:11:10 -0400

    Create routes folders and files, add to server

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // dependencies
 const express = require('express');
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // variables
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // initialize server
 const app = express();
 
+// make public files readily available
+app.use(express.static('public'));
 
+// parse incoming JSON data
+app.use(express.json());
 
+// include all routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // server listener
 app.listen(PORT, () => {
