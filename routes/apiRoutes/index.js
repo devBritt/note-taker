71ebf5ee4a55968f60cff2ea24ef4e8daf32891c
Britt Carlton <brittanyrc95@gmail.com>
2022-07-29 12:10:15 -0400

    Update post route to create new note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 // dependencies
 const router = require('express').Router();
-
+const { createNewNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 // add route to send notes list
 router.get('/notes', (req, res) => {
@@ -11,7 +12,17 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     let notesList = notes;
-    console.log(notesList);
+
+    // validate req.body
+    if (req.body.title && req.body.text) {
+        // set unique id
+        req.body.id = uuidv4();
+        // create new note
+        const note = createNewNote(req.body, notesList);
+        res.json(note);
+    } else {
+        res.status(400).send("The note couldn't be created. It looks like there's missing input...")
+    };
 });
 
 module.exports = router;
